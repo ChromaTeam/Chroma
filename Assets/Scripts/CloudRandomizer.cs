8b5ca1e09a8e88c0cabe82af8307d2d3e558b69f
Mehdi Benadel <mehdi.benadel@gmail.com>
2016-04-16 21:28:44 +0200

    Update Level Update Animations

---
+++ Assets/Scripts/CloudRandomizer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CloudRandomizer : StateMachineBehaviour {
+
+	private Animator m_animator;
+
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		Debug.Log("State Entered !");
+		
+		m_animator = animator;
+		
+		//StartCoroutine("StartAnim");
+	}
+	
+	private void StartAnim() {
+		//yield return new WaitForSeconds(10 * Math.Random());
+		
+		Debug.Log("Start Called !");
+		m_animator.SetTrigger("Start");
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
