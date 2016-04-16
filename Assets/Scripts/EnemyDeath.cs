6a1058c35b28ca5483d87e7c0002dfeb8d74b41d
Mehdi Benadel <mehdi.benadel@gmail.com>
2016-04-16 12:59:32 +0200

    Add Player Shoot mechanic

---
+++ Assets/Scripts/EnemyDeath.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDeath : MonoBehaviour {
+
+    private bool isDying;
+    private Color color;
+    private Transform transform;
+    private Renderer renderer;
+    
+    public Material deathMaterial;
+    private int tick = 0;
+    public AudioClip deathSound;
+    private AudioSource audioSource;
+    
+    void Start() {
+        renderer = GetComponent<Renderer>();
+        transform = gameObject.transform;
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    void Update() {
+        if(isDying) {
+            Color color = renderer.material.GetColor("_TintColor");
+            
+            if(color.a <= 0) {
+                isDying = false;
+                color.a = 0;
+                
+                FinallyDestroy();
+                
+                return;
+            }
+            
+            switch (tick)
+            {
+                case 0:
+                    gameObject.transform.position = transform.TransformPoint(0.1f,0,0);
+                    break;
+                    
+                case 1:
+                    gameObject.transform.position = transform.TransformPoint(0,0.1f,0);
+                    break;
+                    
+                case 2:
+                    gameObject.transform.position = transform.TransformPoint(-0.1f,0,0);
+                    break;
+                    
+                case 3:
+                    gameObject.transform.position = transform.TransformPoint(0,-0.1f,0);
+                    break;
+                    
+                default:
+                    tick = -1;
+                    break;
+            }
+            
+            tick++;
+            
+            color.a -= Time.deltaTime * 0.5f;
+        
+            renderer.material.SetColor("_TintColor",color);
+        }
+    }
+
+	public IEnumerator Dying () {
+        if (!isDying) {
+            renderer.material = deathMaterial;
+            audioSource.PlayOneShot(deathSound,1.0f);
+            isDying = true;
+        }
+            
+        yield return "Dying";
+    }
+    
+    private void FinallyDestroy() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ShootCollision.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootCollision : MonoBehaviour {
+
+    public LayerMask hitLayers;
+	public GameObject explosionPrefab;
+	public AudioClip explosionSound;
+
+	void OnCollisionEnter2D (Collision2D col) {
+		Debug.Log("Object hit !");
+		ExplosionSound();
+		ExplosionWork(col);
+		
+		Destroy(gameObject);
+	}
+	
+	private void ExplosionSound() {
+		AudioSource.PlayClipAtPoint(explosionSound, gameObject.transform.position);
+	}
+
+	private void ExplosionWork (Collision2D col) {
+		GameObject explosion = (GameObject) Instantiate(explosionPrefab,transform.position,Quaternion.identity);
+		Destroy(explosion, 3.0f);
+		
+		Collider2D collider = col.collider;
+		
+		if(collider.CompareTag("Enemy")) {
+			EnemyDeath enemyDeath = collider.gameObject.GetComponent<EnemyDeath>();
+			if(enemyDeath) {
+				StartCoroutine(enemyDeath.Dying());
+			}
+		}
+	}
+}
