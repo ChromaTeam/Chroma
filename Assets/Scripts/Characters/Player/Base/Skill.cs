4637e3742df9d1c4478c9b426b675235540f39c2
Vincent <vincentiu.adam@gmail.com>
2016-04-16 16:33:22 +0200

    Improved jump skill to not continuously jump
    Separated jump and movement skill
    
    Player can now gain and lose skills using the g and l buttons
    
    Created some extra Feet layers
    
    Integrated player animation

---
+++ Assets/Scripts/Characters/Player/Base/Skill.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skill : MonoBehaviour 
+{
+	//used for debugging purposes to see if script is enabled/disabled
+	protected virtual void Start()
+	{
+
+	}
+}
