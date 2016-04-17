7c13b859f4f149130693d509c6c79f1315bf5478
jacqueslelezard <jacques.le.lezard@gmail.com>
2016-04-17 12:30:04 +0200

    Boss pattern + minion animation loop

--- Assets/Scripts/BossArm.cs
+++ Assets/Scripts/BossArm.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class BossArm : MonoBehaviour {
 
+    //coup de poing rapide
     public bool attack1 = true;
+    //coup de poing marteau
     public bool attack2 = false;
+    //fausse attaque
     public bool attack3 = false;
+    //chute de pierre
+    public bool attack4 = false;
     private int timer = 0;
     private GameObject player;
     public GameObject rock;
@@ -58,7 +63,7 @@ public class BossArm : MonoBehaviour {
             }
             else if (timer < 60)
             {
-                HammerAttack(-49,-49);
+                HammerAttack(-49,-51);
                 timer++;
             }
             else if (timer < 100)
@@ -75,6 +80,30 @@ public class BossArm : MonoBehaviour {
         }
         else if (attack3)
         {
+            if (timer < 40)
+            {
+                HammerAttack(1, 6.5f);
+                timer++;
+            }
+            else if (timer < 45)
+            {
+                HammerAttack(-49, -51);
+                timer++;
+            }
+            else if (timer < 100)
+            {
+                HammerAttack(2.5f, 0);
+                timer++;
+            }
+            else
+            {
+                timer = 0;
+                attack3 = false;
+                attack4 = true;
+            }
+        }
+        else if (attack4)
+        {
             if (!invoked)
             {
                 InvokeAttack();
@@ -83,8 +112,9 @@ public class BossArm : MonoBehaviour {
             if(timer > 300)
             {
                 timer = 0;
-                attack3 = false;
+                attack4 = false;
                 attack1 = true;
+                invoked = false;
             }
             else
             {
@@ -96,12 +126,12 @@ public class BossArm : MonoBehaviour {
 
     }
 
-    public void HorizontalAttack(int direction, int hauteur)
+    public void HorizontalAttack(float direction, float hauteur)
     {
         Debug.Log("boss attack 1");
         transform.Translate(direction * Time.deltaTime, hauteur * Time.deltaTime, 0);
     }
-    public void HammerAttack(int direction, int hauteur)
+    public void HammerAttack(float direction, float hauteur)
     {
         Debug.Log("boss attack 2");
         transform.Translate(direction * Time.deltaTime, hauteur * Time.deltaTime, 0);
