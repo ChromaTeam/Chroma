d7b9e8197bedf96b96019aac3903e8c07a61f928
jacqueslelezard <jacques.le.lezard@gmail.com>
2016-04-17 10:02:13 +0200

    boss elements

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss : MonoBehaviour {
+
+    public GameObject player;
+    public int bossLives = 1000;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Boss ready");
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
