415de66cb698231ff6539ab8ec5c361303c8660e
Vincent <vincentiu.adam@gmail.com>
2016-04-20 07:34:48 +0200

    Added Boss Theme

---
+++ Assets/Scripts/BossMusic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossMusic : MonoBehaviour 
+{
+	[SerializeField]
+	private AudioSource m_AudioSource;
+
+	[SerializeField]
+	private SetParameter m_MusicParameter;
+
+	public void OnPlayerTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag.Equals("Player"))
+		{
+			Play();
+		}
+	}
+
+	private void Play()
+	{
+		m_AudioSource.Play();
+		m_MusicParameter.Play();
+	}
+}
